795d4b59375ba5692d3161a4190e36f8a85f7d7f
dreamgonfly <dreamgonfly@gmail.com>
2018-07-21 17:32:38 +0900

    Delete legacy files

--- utils/helper.py
+++
@@ -1,6 +0,0 @@
-from slackclient import SlackClient
-
-poza_token = 'xoxp-210889208129-213610419124-214877724161-655e79305dd8ed333466196de18ded51'
-sc = SlackClient(poza_token)
-def slack(message, channel='#bot'):
-    sc.api_call('chat.postMessage', channel = channel, text = message)
