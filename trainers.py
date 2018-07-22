37fe8a103b279cceddcb6671a9a53e51bf58a76c
dreamgonfly <dreamgonfly@gmail.com>
2018-07-22 21:11:58 +0900

    Upgrade trainer

--- trainers.py
+++ trainers.py
@@ -8,7 +8,7 @@ from os.path import dirname, abspath, join, exists
 BASE_DIR = dirname(abspath(__file__))
 
 
-class BaseTrainer:
+class LanguageModelTrainer:
 
     def __init__(self, model, train_dataloader, val_dataloader, loss_function, optimizer,
                  device=None, print_every=1, save_every=100, logger=None, save_name=None):
@@ -27,8 +27,6 @@ class BaseTrainer:
         self.epoch = 0
         self.train_epoch_losses = []
         self.val_epoch_losses = []
-        # self.train_epoch_accuracies = []
-        # self.val_epoch_accuracies = []
 
         self.logger = logger
 
@@ -38,7 +36,7 @@ class BaseTrainer:
         self.model.train()
 
         train_batch_losses = []
-        # train_batch_losses, train_batch_metrics = [], []
+        train_batch_token_counts = []
         for train_targets, train_inputs in tqdm(self.train_dataloader):
             train_inputs, train_targets = train_inputs.to(self.device), train_targets.to(self.device)
 
@@ -55,21 +53,19 @@ class BaseTrainer:
             self.optimizer.step()
 
             train_batch_losses.append(train_batch_loss.item())
-
-            # train_batch_metric = self._calculate_metric(train_outputs, train_targets)
-            # train_batch_metrics.append(train_batch_metric)
+            train_batch_token_count = train_targets_flat.size(0)
+            train_batch_token_counts.append(train_batch_token_count)
 
             if self.epoch == 0:  # for debugging
                 break
 
-        train_data_size = len(self.train_dataloader.dataset)
-        train_epoch_loss = sum(train_batch_losses) / train_data_size
-        # train_epoch_accuracy = sum(train_batch_metrics).item() / train_data_size
+        train_token_counts = sum(train_batch_token_counts)
+        train_epoch_loss_per_token = sum(train_batch_losses) / train_token_counts
 
         if epoch % self.print_every == 0:
             self.model.eval()
             val_batch_losses = []
-            # val_batch_losses, val_batch_metrics = [], []
+            val_batch_token_counts = []
             for val_targets, val_inputs in self.val_dataloader:
                 val_inputs, val_targets = val_inputs.to(self.device), val_targets.to(self.device)
 
@@ -81,37 +77,32 @@ class BaseTrainer:
                 val_batch_loss = self.loss_function(val_outputs_flat, val_targets_flat)
 
                 val_batch_losses.append(val_batch_loss.item())
+                val_batch_token_count = val_targets_flat.size(0)
+                val_batch_token_counts.append(val_batch_token_count)
 
-                # val_batch_metric = self._count_corrects(val_outputs, val_targets)
-                # val_batch_metrics.append(val_batch_metric)
-
-            val_data_size = len(self.val_dataloader.dataset)
-            val_epoch_loss = sum(val_batch_losses) / val_data_size
-            # val_epoch_accuracy = sum(val_batch_metrics).item() / val_data_size
+            val_token_counts = sum(val_batch_token_counts)
+            val_epoch_loss_per_token = sum(val_batch_losses) / val_token_counts
         else:
-            val_epoch_loss = 0
-            # val_epoch_accuracy = None
+            val_epoch_loss_per_token = None
 
-        return train_epoch_loss, val_epoch_loss  # , train_epoch_accuracy, val_epoch_accuracy
+        return train_epoch_loss_per_token, val_epoch_loss_per_token
 
     def run(self, epochs=10):
 
         for epoch in range(self.epoch, epochs + 1):
             self.epoch = epoch
 
-            train_epoch_loss, val_epoch_loss = self.train(epoch)  # , train_epoch_accuracy, val_epoch_accuracy
+            train_epoch_loss_per_token, val_epoch_loss_per_token = self.train(epoch)
 
-            self.train_epoch_losses.append(train_epoch_loss)
-            self.val_epoch_losses.append(val_epoch_loss)
-            # self.train_epoch_accuracies.append(train_epoch_accuracy)
-            # self.val_epoch_accuracies.append(val_epoch_accuracy)
+            self.train_epoch_losses.append(train_epoch_loss_per_token)
+            self.val_epoch_losses.append(val_epoch_loss_per_token)
 
-            train_epoch_perplexity = self._calculate_perplexity(train_epoch_loss)
-            val_epoch_perplexity = self._calculate_perplexity(val_epoch_loss)
+            train_epoch_perplexity = self._calculate_perplexity(train_epoch_loss_per_token)
+            val_epoch_perplexity = self._calculate_perplexity(val_epoch_loss_per_token)
 
             if epoch % self.print_every == 0 and self.logger:
-                self._leave_log(train_epoch_loss, val_epoch_loss, train_epoch_perplexity,
-                                val_epoch_perplexity, progress=epoch / epochs)
+                self._leave_log(train_epoch_loss_per_token, val_epoch_loss_per_token,
+                                train_epoch_perplexity, val_epoch_perplexity, progress=epoch / epochs)
 
             if epoch % self.save_every == 0:
                 if self.logger:
@@ -156,7 +147,7 @@ class BaseTrainer:
         if not exists(checkpoint_dir):
             os.mkdir(checkpoint_dir)
 
-        base_filename = '{model_name}-{start_time}-{epoch}.pth'
+        base_filename = '{model_name}_{epoch}_{start_time}.pth'
         checkpoint_filename = base_filename.format(model_name=self.save_name,
                                                    start_time=self.start_time,
                                                    epoch=self.epoch)
@@ -168,18 +159,24 @@ class BaseTrainer:
             self.best_checkpoint_filepath = checkpoint_filepath
 
 
-# TODO : Make sane collate_fn
-def collate_fn(batch):
-    """merges a list of samples to form a mini-batch."""
+def inputs_targets_collate_fn(batch):
+    """merges a list of samples to form a mini-batch.
 
-    text_lengths = [sentence.size(0) for sentiment_index, sentence in batch]
-    longest_length = max(text_lengths)
+    Args:
+        batch : tuple of inputs and targets. For example,
+        ([1, 164, 109, 253, 66, 484, 561, 76, 528, 279, 458],
+        [164, 109, 253, 66, 484, 561, 76, 528, 279, 458, 1])
+    """
 
-    reviews_padded = [torch.cat([torch.zeros(longest_length - sentence.size(0), 128), sentence], dim=0) for
-                      sentiment_index, sentence in
-                      batch]
-    labels = [sentiment_index for sentiment_index, sentence in batch]
+    seq_lengths = [len(inputs) for inputs, targets in batch]
+    longest_length = max(seq_lengths)
 
-    reviews_tensor = torch.stack(reviews_padded)
-    labels_tensor = torch.stack(labels)  # classification
-    return labels_tensor, reviews_tensor
+    PAD_INDEX = 0
+
+    inputs_padded = [inputs + [PAD_INDEX] * (longest_length - len(inputs)) for inputs, targets in batch]
+    targets_padded = [targets + [PAD_INDEX] * (longest_length - len(targets)) for inputs, targets in batch]
+
+    inputs_tensor = torch.tensor(inputs_padded)
+    targets_tensor = torch.tensor(targets_padded)
+
+    return inputs_tensor, targets_tensor
